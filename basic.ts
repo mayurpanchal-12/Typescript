c2b49d82d02d42998d15a7990dd1f5b994debcd3
maayurpanchal-12 <panchalmayur2004@gmail.com>
2026-03-30 23:50:32 +0530

    updates in basic code

--- basic.ts
+++ basic.ts
@@ -1,6 +1,23 @@
-function greet(name: string): string {
-    return "Hello " + name;
-}
+function greet(name:string):string{
+    return "hello" + name;
+};
 
-const message = greet("Mayur");
-console.log(message);
+console.log(greet("mayur"));  // will log "hello mayur"
+// console.log(greet(56));  // will give error as 56 is not of type string
+
+
+
+
+
+
+
+
+/*
+function greet(name){
+     return "Hello, " + name + "!";
+ };
+ console.log(greet("mayur"));
+console.log(greet(56));  
+
+ both will log in js as js does not have type checking, Ts have Type checking
+ */
