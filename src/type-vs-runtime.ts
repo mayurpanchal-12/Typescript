0ba04aaddb761a1db8325ecedb9335cc0353f289
maayurpanchal-12 <panchalmayur2004@gmail.com>
2026-04-01 23:17:43 +0530

    type-vs-runtime

---
+++ src/type-vs-runtime.ts
@@ -0,0 +1,18 @@
+let orders = ['12', '34', '56'];
+
+let currentOrder;  
+// TypeScript infers type as `any` (implicit any)
+// At runtime, since no value is assigned, it is `undefined`
+
+console.log(typeof(currentOrder)); // "undefined"
+
+for (let order of orders) {
+   if (order === '34') {
+      currentOrder = order;  
+      // Runtime value becomes a string ("34")
+      // But TypeScript type is still `any` (no type safety)
+      break;
+   }
+}
+
+console.log(typeof(currentOrder), currentOrder); //if value found in orders, it will be   "string", "34" else "undefined", undefined
